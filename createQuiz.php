ee937360365c706c0d8d3a4910ceb24dcbf196ac
KevinLarkin <KevinLarkin@Kevins-MacBook-Pro.local>
2015-07-28 12:51:41 -0400

    updated

--- createQuiz.php
+++ createQuiz.php
@@ -6,6 +6,7 @@
 ?>
 <style>
 th,td {padding:5px;}
+//if {display: inline}
 </style>
 <?php
 
@@ -17,7 +18,7 @@ curl_setopt($ch,CURLOPT_POST,true);
 curl_setopt($ch,CURLOPT_POSTFIELDS,$dataString);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $send=curl_exec($ch);
-echo $send;
+//echo $send;
 //echo count($send);
 $questions= json_decode($send);
 //echo count($questions);
@@ -29,8 +30,7 @@ echo  "Insert Exam Name Here<br/>";
 echo  "<input type=\"text\" name=\"ExamName\">";
 echo  "<br/>";
 echo  "<input type=\"hidden\" name=\"cmd\" value=\"createExam\">";
-
-echo    "<table border=\"1\">";
+echo    "<table border=\"1\ class=\"inlineTable\" style=\"float:left\">";
 echo    "<tr>";
 echo    "<th>Selcct Question</th>";
 echo    "<th>Question</th>" ;
@@ -40,22 +40,39 @@ echo "</tr>";
     for ($i = 0; $i < count($questions); $i++) {
       echo "<tr>";
       echo "<td><input type=\"checkbox\" name=\"q".$i."\" value=\"".$questions[$i]->{qid}."\" ></td>";
-      //echo "<td>".$questions[$i]->{qid}."</td>";
-      //echo "<td>".$questions[$i]->{quesion}."</td>";
       echo "<td>".$questions[$i]->{"question"}."<br></td>";
       echo "<td>".$questions[$i]->{weight}."</td>";
       echo "<td>".$questions[$i]->{type}."</td>";
       echo "</tr>";
   }
+  echo "<input name=\"submit\" type=\"submit\" value=\"Submit\" style=\"float:left\>";
     echo"</table>";
-
-/*for ($i=0; $i< count($questions); $i++){
-    echo "<input type=\"checkbox\" name=\"q".$i."\" value=\"".$questions[$i]->{qid}."\" >";
-    echo $questions[$i]->{"question"}."<br>";
-}*/
-
-echo "<input name=\"submit\" type=\"submit\" value=\"Submit\">";
+//echo "<input name=\"submit\" type=\"submit\" value=\"Submit\" style=\"float:left\>";
 echo "</form>";}else{
     echo "There are no Questions to make a Quiz";
+}
+
+if(count($questions)>0){
+echo  "<form action= \"addQuiz.php\" method=\"POST\">";
+//echo  "<br/>";
+//echo  "Insert Exam Name Here<br/>";
+//echo  "<input type=\"text\" name=\"ExamName\">";
+//echo  "<br/>";
+echo  "<input type=\"hidden\" name=\"cmd\" value=\"createExam\">";
+echo    "<table border=\"1\ class=\"inlineTable\" style=\"float:right\">";
+echo    "<tr>";
+echo    "<th>Selcct Test</th>";
+echo    "<th>Test</th>" ;
+echo "</tr>";
+    for ($i = 0; $i < count($questions); $i++) {
+      echo "<tr>";
+      echo "<td><input type=\"checkbox\" name=\"q".$i."\" value=\"".$questions[$i]->{qid}."\" ></td>";
+      echo "<td>".$questions[$i]->{"test"}."<br></td>";
+      echo "</tr>";
+  }
+    echo"</table>";
+echo "<input name=\"submit\" type=\"submit\" value=\"Submit\" style=\"float:right\">";
+echo "</form>";}else{
+    echo "There are no Questions to make a test";
 }
         ?>
